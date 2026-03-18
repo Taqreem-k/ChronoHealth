a3463428aebd453c3dd86534ae70cd85615e35c7
Taqreem-k <md.taqreemkhan@gmail.com>
2026-03-18 21:22:59 +0530

    feat: added PyPDFLoader for document ingestion and implemented recursive character text splitting

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 
 st.title("ChronoHealth")
 
@@ -7,8 +9,19 @@ st.header("About:")
 st.subheader("ChronoHealth is an Agentic Personal Health Record System")
 
 uploaded_files = st.file_uploader(
-    "Upload images", type = ["pdf"]
+    "Upload PDF", type = ["pdf"]
 )
 
 if uploaded_files is not None:
     st.success("File successfully uploaded!")
+
+    with open("temp.pdf", "wb") as f:
+        f.write(uploaded_files.getvalue())
+    
+    loader = PyPDFLoader("temp.pdf")
+    pages = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=200)
+    chunks = text_splitter.split_documents(pages)
+
+    st.write(f"Document succesfully split into {len(chunks)} chunks.")
